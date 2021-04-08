f89212f5c4c60a0eb5913dd449e797a3d58346a2
MortenWillendrup <Johansen88@gmail.com>
2021-04-08 12:05:58 +0200

    Minor Update to the loop

--- Applied_Finance/Model.py
+++ Applied_Finance/Model.py
@@ -1,4 +1,7 @@
 
+from linearmodels.datasets import french
+
+data = french.load()
 
 # Modules
 
@@ -13,46 +16,51 @@ funds = pd.read_excel('USmutual.xlsx')
 funds_list = []
 funds_list = funds['Morningstar Category'].unique()
 funds_list = funds_list.tolist()
-funds_list = [i for i in funds_list if type(i) is not int]
-funds_list = [i for i in funds_list if str(i) != 'nan']
-funds_list = [fund.replace(" ","_").replace("-","_") for fund in funds_list]
+funds_list = [i for i in funds_list if type(i) is str]
+
 
 for fund in funds_list:
     print(fund)
 
+fund_dict = {}
+
 
 # Creating DataFrames for each Category
 for fund in funds_list:
-    vars()[fund.replace(" ","_").replace("-","_")] = funds[(funds['Morningstar Category'] == fund) & (funds['2010-01-01'].notnull()) &
-                         (funds['Ticker'].notnull())]
+    fund_dict[fund.replace(" ","_").replace("-","_")] = vars()[fund.replace(" ","_").replace("-","_")] = \
+        funds[(funds['Morningstar Category'] == fund) &
+              (funds['2010-01-01'].notnull()) &
+              (funds['Ticker'].notnull())]
+
+funds_list = [fund.replace(" ","_").replace("-","_") for fund in funds_list]
+#
+# print(US_Fund_Large_Value.info)
+#
+#
+#
+# US_Fund_Large_Value = US_Fund_Large_Value.iloc[:, 29:280]
 
 
+#CLeaning for performance data
+for fund in fund_dict:
+    fund_dict[fund] = fund_dict[fund].iloc[:, 29:280]
 
 
+####  - Reset from here
+    fund.drop(fund.columns[136:540], inplace=True, axis =1)
+    fund.drop(fund.columns[0:1], inplace=True, axis =1)
+    fund = fund.T
+    fund.drop(fund.columns[134:160], inplace=True, axis =0)
 
+    new_header = fund.iloc[0]
 
-Large_cap_funds.to_csv('Large_cap_funds.csv', index=True)
+    fund = fund[1:]
 
-Large_cap_funds = pd.read_csv('Large_cap_funds.csv',index_col=0)
+    fund.columns = new_header
 
+    fund = fund.dropna(axis=1)
 
-
-
-Large_cap_funds.drop(Large_cap_funds.columns[2:147], inplace=True, axis =1)
-Large_cap_funds.drop(Large_cap_funds.columns[136:540], inplace=True, axis =1)
-Large_cap_funds.drop(Large_cap_funds.columns[0:1], inplace=True, axis =1)
-Large_cap_funds_transposed = Large_cap_funds.T
-Large_cap_funds_transposed.drop(Large_cap_funds.columns[134:160], inplace=True, axis =0)
-
-new_header = Large_cap_funds_transposed.iloc[0]
-
-df = Large_cap_funds_transposed[1:]
-
-df.columns = new_header
-
-df_good_data = df.dropna(axis=1)
-
-df_good_data.to_csv('Large_funds_cleaned.csv', index=True)
+# df_good_data.to_csv('Large_funds_cleaned.csv', index=True)
 from dateutil.relativedelta import *
 
 
